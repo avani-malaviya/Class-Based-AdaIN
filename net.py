af0422bc9b98eb33127cf8ce8a9af1d1d64b2dde
avani-malaviya <avani.malaviya@gmail.com>
2024-07-02 11:32:02 +0200

    using pymatting training trail

--- net.py
+++ net.py
@@ -165,13 +165,14 @@ class Net(nn.Module):
 
         for i in range(batch_size):
             single_output = output[i]
-            output_np = single_output.detach().cpu().numpy()
+            output_np = np.transpose(single_output.detach().cpu().numpy())
             laplacian = lkm_laplacian(output_np)
             laplacian_tensor = torch.from_numpy(laplacian).to(output.device)
             reg_loss = torch.sum(single_output * laplacian_tensor * single_output)
             total_reg_loss += reg_loss
 
         avg_reg_loss = total_reg_loss / batch_size
+
         return avg_reg_loss
 
 
