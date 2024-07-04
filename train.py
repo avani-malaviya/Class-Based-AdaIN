788503d2669edd88da697977d0ac6264bf46c486
avani-malaviya <avani.malaviya@gmail.com>
2024-07-04 09:41:45 +0200

    finetuning

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ parser.add_argument('--log_dir', default='./logs',
 parser.add_argument('--lr', type=float, default=1e-4)
 parser.add_argument('--lr_decay', type=float, default=5e-5)
 parser.add_argument('--max_iter', type=int, default=200000)
-parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--batch_size', type=int, default=4)
 parser.add_argument('--style_weight', type=float, default=1.0)
 parser.add_argument('--content_weight', type=float, default=10.0)
 #parser.add_argument('--reg_weight', type=float, default=1000.0)
