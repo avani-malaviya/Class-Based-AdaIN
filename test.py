79874dd719b54e682084831dea7acd694a54c3fc
avani-malaviya <avani.malaviya@gmail.com>
2024-07-15 14:54:32 +0200

    using sd vae

--- test.py
+++ test.py
@@ -121,12 +121,11 @@ def decode_image(latent, original_size, keep_square=False):
     return image
 
 
-def style_transfer(adain, vgg, decoder, content_f, content_sem, style = None, style_sem = None, style_means = None, style_stds = None, alpha=1.0):
+def style_transfer(adain, content_f, content_sem, style_f = None, style_sem = None, style_means = None, style_stds = None, alpha=1.0):
     assert (0.0 <= alpha <= 1.0)
     if ((style_means is not None) and (style_stds is not None)):
         feat = adain(content_f, content_sem, style_means, style_stds)
     elif ((style is not None) and (style_sem is not None)):
-        style_f = vgg(style)
         feat = adain(content_f, style_f, content_sem, style_sem)
     else: 
         print("Incomplete Style Data")
@@ -262,8 +261,9 @@ for content_path in content_paths:
             style_sem = style_sem.to(device).unsqueeze(0)
 
             content_f = encode_image(content_path)
+            style_f = encode_image(style_path)
             with torch.no_grad():
-                adain_feat = style_transfer(adain, content_f, content_sem, style_means=style_means, style_stds=style_stds, alpha=args.alpha)
+                adain_feat = style_transfer(adain, content_f, content_sem, style_f=style_f, style_sem=style_sem, alpha=args.alpha)
             output = decode_image(adain_feat)
 
             output_name = output_dir / '{:s}_stylized_{:s}{:s}'.format(
